20dd2af642a995be0b7a84f73c6ae5b8a11d2d81
Jakub Matłacz <git@github.com:matlaczj/mjrep.git>
2020-03-16 12:43:02 +0100

    rezygnacja z ultraoptymalizacji prepare_space.c bo wykonuje sie 1 raz

--- prepare_space.c
+++ prepare_space.c
@@ -9,50 +9,35 @@ void prepare_space(int r, int c, cell_t space[r][c])
     // we wont need state or next_state for padding but we dont want random values there
     //
 
-    //troche zmienilem kod, zeby dla kazdej komorki nie sprawdzalo 4 warunkow
-
+    // nie ma moim zdaniem sensu optymalizowac tak mocno czegos co sie wykonuje tylko 1 raz w programie
+    // a bardzo mocno spada czytelnosc i mozliwosc bledu wzrasta a zysk znikomy, zmienilem tez troche debugging 
+    // bo wydawal mi sie dziwny, teraz chodzi o sprawdzenie czy jakims cudem jakas niemozliwa wartosc nie dostala sie do ktorejs // komorki 
     int i,j;
-       for(j=0; j<c; j++) {
-        space[0][j].state = IRR; 
-        space[0][j].next_state = IRR; 
-        space[0][j].is_padding = ON;
-        space[r-1][j].state = IRR; 
-        space[r-1][j].next_state = IRR; 
-        space[r-1][j].is_padding = ON;
-    }
-
-    for(i=1; i<r-1; i++) {
-        space[i][0].state = IRR; 
-        space[i][0].next_state = IRR; 
-        space[i][0].is_padding = ON;
-        space[i][c-1].state = IRR; 
-        space[i][c-1].next_state = IRR; 
-        space[i][c-1].is_padding = ON;
-
-        for(j=1; j<c-1; j++)
+    for(i=0; i<r; i++)
+        for(j=0; j<c; j++)
         {
-/*            if(i==0 || i==r-1  || j==0 || j==c-1 )
+            if(i==0 || i==r-1 || j==0 || j==c-1)
             {
-                space[i][j].state = OFF; 
-                space[i][j].next_state = OFF; 
+                space[i][j].state = IRR; 
+                space[i][j].next_state = IRR; 
                 space[i][j].is_padding = ON;
             }
             else
-            {   */
-            space[i][j].state = OFF;
-            space[i][j].next_state = OFF; 
-            space[i][j].is_padding = IRR;
-//            }
-#ifdef DEBUG            
-            if(space[i][j].state != OFF && space[i][j].state != IRR)
-                printf("\nspace[%d][%d].state has wrong value: %d\n",i,j,space[i][j].state);
-            if(space[i][j].next_state != OFF && space[i][j].next_state != IRR)
-                printf("\nspace[%d][%d].next_state has wrong value: %d\n",i,j,space[i][j].next_state);
-            if(space[i][j].is_padding != IRR && space[i][j].is_padding != ON )
-                printf("\nspace[%d][%d].is_padding has wrong value: %d\n",i,j,space[i][j].next_state);
-#endif            
+            {
+                space[i][j].state = OFF;
+                space[i][j].next_state = OFF; 
+                space[i][j].is_padding = IRR;
+            }
+            
+            #ifdef DEBUG 
+            if(space[i][j].state != OFF & space[i][j].state != ON && space[i][j].state != IRR )
+                printf("\nspace[%d][%d].state has impossible value: %d\n",i,j,space[i][j].state);
+            if(space[i][j].next_state != OFF & space[i][j].next_state != ON && space[i][j].next_state != IRR )
+                printf("\nspace[%d][%d].state has impossible value: %d\n",i,j,space[i][j].next_state);
+            if(space[i][j].is_padding != OFF & space[i][j].is_padding != ON && space[i][j].is_padding != IRR )
+                printf("\nspace[%d][%d].state has impossible value: %d\n",i,j,space[i][j].is_padding);
+            #endif
         }
-    }
 }
 
 
